4c8b6eb00116a2c5c780de79115d72eaa62bf738
JadeTyrer <jadetyrer@hotmail.co.uk>
2020-08-06 13:42:59 +1000

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import LandingPage from './components/LandingPage'
+import GlobalStyle from './globalStyles/fonts'
+
+
 
 const App = () => {
   return (
     <div >
+    <GlobalStyle />
           <LandingPage />
     </div>
   )
