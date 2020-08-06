aa03e485975b79936ee6f4281eb1e89806717ac3
JadeTyrer <jadetyrer@hotmail.co.uk>
2020-08-06 13:32:54 +1000

    added styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import LandingPage from './components/LandingPage'
 
 const App = () => {
   return (
     <div >
-          The basic empty template
+          <LandingPage />
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import { NavContainer, NavLogo, BurgerIcon, BurgerLabel } from './StyledComponents';
-import WebNav from './WebNav'
+import {NavLogo, NavContainer, NavLink, NavGroupDiv, ContactDiv} from './StyledComponents';
 
 
 const Navbar = () => {
   return (
     <NavContainer>
       <NavLogo src="/txo_logo_2.svg" alt="txo logo"></NavLogo>
-      <BurgerIcon>
-        <BurgerLabel for="toggle">&#9776;</BurgerLabel>
-        <input type="checkbox" style={{display: "none"}}/>
-      </BurgerIcon>
-      <WebNav />
+      <ContactDiv>
+      <NavGroupDiv>
+        <NavLink style={{marginBottom: "10px"}}>ENQUIRIES</NavLink>
+        <NavLink>General</NavLink>
+        <NavLink>+44 (0) 020 3613 4733</NavLink>
+        <NavLink>Info@txowork.com</NavLink>
+      </NavGroupDiv>
+      <NavGroupDiv>
+        <NavLink></NavLink><br></br>
+        <NavLink>Sales</NavLink>
+        <NavLink>+44 (0) 020 3613 4733</NavLink>
+        <NavLink>Info@txowork.com</NavLink>
+      </NavGroupDiv>
+      <NavGroupDiv>
+        <NavLink style={{marginBottom: "10px"}}>ADDRESS</NavLink>
+        <NavLink>Morelands</NavLink>
+        <NavLink>5-23 Old Street</NavLink>
+        <NavLink>London EC1V 9HL</NavLink>
+      </NavGroupDiv>
+      <NavGroupDiv>
+        <NavLink style={{marginBottom: "10px"}}>CONNECT</NavLink>
+        <NavLink>Instagram</NavLink>
+        <NavLink>LinkedIn</NavLink>
+        <NavLink>Facebook</NavLink>
+      </NavGroupDiv>
+    </ContactDiv>
     </NavContainer>
   )
 }
